92f3851af0a8a506d2ad13f5768fe0fea06b758c
Abhishek <atj127@gmail.com>
2019-03-31 16:31:46 -0400

    preliminary gabor feature implementation

--- proposedAlgorithm.py
+++ proposedAlgorithm.py
@@ -2,14 +2,17 @@ import cv2
 import numpy as np
 import sys
 from matplotlib import pyplot as plt
+import math
 
 def get_input_image(path_to_image):
     image = cv2.imread(path_to_image)
     image_clustering(image)
+    gabor_filter(image)
     return image
 
 def image_clustering(image):
     clustered_image = image.copy()
+    clustered_image = cv2.cvtColor(clustered_image, cv2.COLOR_BGR2GRAY)
     Z = clustered_image.reshape((-1,3))
 
     # convert to np.float32
@@ -53,5 +56,43 @@ def plot_clustered_data(img):
     plt.xlabel('Height'),plt.ylabel('Weight')
     plt.show()
 
+
+def check_aspect_ratio(image):
+    height = image.shape[0]
+    width = image.shape[1]
+
+    if float(height/width) > 1.9 or float(height/width) < float(1/1.9):
+        return False
+
+    return True
+
+def gabor_filter(image):
+    height = image.shape[0]
+    width = image.shape[1]
+    g_kernel = cv2.getGaborKernel((21, 21), 8.0, np.pi/4, 10.0, 0.5, 0, ktype=cv2.CV_32F)
+
+    # img = cv2.imread('test.jpg')
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    # img = image.copy()
+    filtered_img = cv2.filter2D(img, cv2.CV_8UC3, g_kernel)
+
+    cv2.imshow('image', img)
+    cv2.imshow('filtered image', filtered_img)
+
+    h, w = g_kernel.shape[:2]
+    # g_kernel = cv2.resize(g_kernel, (3*w, 3*h), interpolation=cv2.INTER_CUBIC)
+    g_kernel = cv2.resize(filtered_img, (3*w, 3*h), interpolation=cv2.INTER_CUBIC)
+    cv2.imshow('gabor kernel (resized)', g_kernel)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return
+
+def gabor2(image):
+    kernel = cv2.getGaborKernel((21, 21), 5, 1, 10, 1, 0, cv2.CV_32F)
+    kernel /= math.sqrt((kernel * kernel).sum())
+    filtered = cv2.filter2D(image, -1, kernel)
+    cv2.imshow('Abhishek', filtered)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 get_input_image('yield.jpg')
